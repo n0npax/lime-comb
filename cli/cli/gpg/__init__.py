130a20f5ffc54b14586735550f526e7ee8608180
Marcin Niemira <marcin.niemira@gmail.com>
2020-01-06 00:25:25 +1100

    fix gpg dep and add encrypt function

---
+++ cli/cli/gpg/__init__.py
@@ -0,0 +1,10 @@
+import gnupg
+gpg = gnupg.GPG(gnupghome=None)
+gpg.encoding = 'utf-8'
+
+def encrypt(emails, data):
+    encrypted_data = gpg.encrypt(data, emails)
+    if not encrypted_data.ok:
+        raise Exception("encryption failed {encrypted_data.stderr}")
+    return str(encrypted_data)
+
