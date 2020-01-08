06c15a86ef098d5b551d70f6456753290a7666b2
Marcin Niemira <marcin.niemira@gmail.com>
2020-01-08 19:36:43 +1100

    generate priv key

--- cli/cli/config/__init__.py
+++ cli/cli/config/__init__.py
@@ -10,15 +10,22 @@ OAUTH_GCP_CONF = "/home/n0npax/workspace/lime-comb/cli/client-lime-comb.json"
 class Config:
     app_name = "lime-comb"
 
-    config_dir = Path(user_config_dir(app_name))
-    config_file = config_dir / "config.yml"
     data_dir = Path(user_data_dir(app_name))
+    config_dir = Path(user_config_dir(app_name))
+
+    config_file = config_dir / "config.yml"
     credentials_file = data_dir / "credentials"
     keyring_dir = data_dir / "keyring"
 
     config_dir.mkdir(exist_ok=True)
     data_dir.mkdir(exist_ok=True)
     keyring_dir.mkdir(exist_ok=True)
+
+    username = "marcin.niemira"
+    email =  "marcin.niemira@gmail.com"
+    comment = "Lime Comb"
+
+    password=None
 
 
 try:
