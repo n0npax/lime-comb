510f6278affcfa2528de7932d6c06560f4330aa8
Marcin Niemira <marcin.niemira@gmail.com>
2020-02-20 18:24:09 +1100

    add test for auth

--- cli/tests/test_auth.py
+++ cli/tests/test_auth.py
@@ -5,21 +5,36 @@ from uuid import uuid4
 import google
 import pytest
 
-from cli.auth.google import get_anon_cred, read_creds, save_creds
+from cli.auth.google import get_anon_cred, read_creds, save_creds, get_cred
 from cli.config import Config
 
 
 class Creds:
-    def __init__(self):
-        self.expired = True
+    def __init__(self, expired=True):
+        self.expired = expired
         self.uuid = uuid4()
 
 
-def test_read_and_save_creds():
-    creds = Creds()
-    save_creds(creds)
+@pytest.fixture
+def valid_cred():
+    return Creds(expired=False)
+
+
+@pytest.fixture
+def invalid_cred():
+    return Creds()
+
+
+def test_read_and_save_creds(valid_cred):
+    save_creds(valid_cred)
     r_creds = read_creds()
-    assert creds.uuid == r_creds.uuid
+    assert valid_cred.uuid == r_creds.uuid
+
+
+def test_get_saved_creds(valid_cred):
+    save_creds(valid_cred)
+    with get_cred(Config.credentials_file) as cred:
+        assert not cred.expired
 
 
 def test_get_anon_creds():
