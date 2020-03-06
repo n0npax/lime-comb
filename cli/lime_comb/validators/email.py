a172e79410599a38da367b5fdcb45142b5400e30
zabaweb <zabaweb@users.noreply.github.com>
2020-03-06 07:30:33 +0100

    Minor stuff (#3)

--- cli/lime_comb/validators/email.py
+++ cli/lime_comb/validators/email.py
@@ -2,6 +2,6 @@ from email_validator import validate_email
 
 
 def lc_validate_email(email):
-    # assume this may be executed behing internal corp proxy
+    # assume this may be executed behind internal corp proxy
     validate_email(email, check_deliverability=False)
     return email
